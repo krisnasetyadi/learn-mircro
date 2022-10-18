69b41a8000316a163c4560a7408d7d38bd0fda02
krisnasetyadi <krisnadwisetyadi@gmail.com>
2022-10-18 21:18:20 +0700

    add axios cors

--- post/index.js
+++ post/index.js
@@ -1,29 +1,39 @@
 const express = require('express')
 const  bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
 const app = express()
 
 // app.use(express.json())
 
 app.use(bodyParser.json())
-
+app.use(cors())
 const posts = {}
-
-console.log('posts', posts)
 
 app.get('/posts', (req,res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req,res) => {
+app.post('/posts', async (req,res) => {
     const id = randomBytes(4).toString('hex')
-    console.log('id', id)
     const { title } = req.body
-    console.log('title', title)
     posts[id] = {
         id, title
     }
+   await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
     res.status(201).send(posts[id])
+})
+
+app.post('/events',(req, res) => {
+    console.log('Received Event',req.body.type)
+
+    res.send({});
 })
 
 app.listen(4000, ()=>{
