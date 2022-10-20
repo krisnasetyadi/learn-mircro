3814d58822e5d3249ae24405ea804a72fdb934f8
krisnasetyadi <krisnadwisetyadi@gmail.com>
2022-10-20 18:23:18 +0700

    add docker

--- eventbus/index.js
+++ eventbus/index.js
@@ -19,6 +19,9 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4002/events', event).catch((err) => console.log(err.message))
     // moderation
     axios.post('http://localhost:4003/events', event).catch((err) => console.log(err.message))
+
+    axios.post('http://localhost:4006/events', event).catch((err) => console.log(err.message))
+    axios.post('http://localhost:4007/events', event).catch((err) => console.log(err.message))
     res.send({ status: 'OK' })
 })
 
